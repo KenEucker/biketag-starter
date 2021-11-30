5ca05a1fa88fd9115f67bf120aa8155180f78c18
Ken Eucker <keneucker@gmail.com>
2021-11-30 00:09:46 -0800

    updating to 1.11.1

--- index.ts
+++ index.ts
@@ -29,7 +29,7 @@ const opts = {
   
   const fetchBikeTags = async (client: BikeTagClient) => {
     /// Get game data from the API
-    let {data: game} = await client.getGame(opts as unknown as getGamePayload)
+    let {data: game} = await client.game(opts as unknown as getGamePayload)
     
     console.info(game ? "\x1b[44mGame Retrieved\x1b[0m" : "\x1b[44mGame Could Not Be Retrieved\x1b[0m", { game })
     
@@ -40,14 +40,14 @@ const opts = {
       console.info("\x1b[44mNew Configuration Set For Additional Sources\x1b[0m", { config })
       
       /// get tag number 1 from different sources
-      const {data: sanityTag1} = await client.getTag(1, {source: 'sanity'})
-      const {data: redditTag1} = await client.getTag(1, {source: 'reddit'})
+      const {data: sanityTag1} = await client.tags(1, {source: 'sanity'})
+      const {data: redditTag1} = await client.tags(1, {source: 'reddit'})
 
       console.info("\x1b[44mTags Fetched From Additional Sources\x1b[0m", { sanityTag1, redditTag1 })
     }
 
     /// get all tags from the default source
-    const {data: tags, source: tagsSource} = await client.getTags()
+    const {data: tags, source: tagsSource} = await client.tags()
 
     console.info("\x1b[44mAll Tags Fetched From Default Source\x1b[0m", { tags: tags.length > 50 ? [tags] : tags, tagsSource })
   }
